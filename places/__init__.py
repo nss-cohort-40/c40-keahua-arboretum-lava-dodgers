e3c8ad29928304f2df21870affcf052134835ef0
Zach Nicholson <raiste.doctow@gmail.com>
2020-07-23 11:17:00 -0500

    joe fixed my imports

--- places/__init__.py
+++ places/__init__.py
@@ -1,6 +1,1 @@
-from .environments import Forest
-from .environments import Coastline
-from .environments import River
-from .environments import Swamp
-from .environments import Grassland
-from .environments import Mountain
+from .environments import *
