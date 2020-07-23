b92ca584a9e1303b00f1a9804b98483d1688d14e
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-07-23 11:59:31 -0500

    we now have imports too

--- plants/__init__.py
+++ plants/__init__.py
@@ -1,3 +1,5 @@
 from .plant import Plant
-
 from .blue_jade import Blue_Jade
+from .mountain_apple import Mountain_Apple
+from .rainbow_eucalyptus import Rainbow_Eucalyptus
+from .silversword import Silversword
