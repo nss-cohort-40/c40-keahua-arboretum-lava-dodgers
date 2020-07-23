e528137b27c18d4844e80ea5823233faf61f04a7
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-07-23 11:52:54 -0500

    we now have plants

---
+++ plants/rainbow_eucalyptus.py
@@ -0,0 +1,6 @@
+from plants import Plant
+
+
+class Rainbow_Eucalyptus(Plant):
+    def __init__(self):
+        Plant.__init__(self, "Rainbow Eucalyptus Tree", "All", 8, ["Forest"], "Low", "Shade")
---
+++ plants/silversword.py
@@ -0,0 +1,6 @@
+from plants import Plant
+
+
+class Silversword(Plant):
+    def __init__(self):
+        Plant.__init__(self, "Silversword", "Fall", 22, ["Grasssland"], "High", "Full")
