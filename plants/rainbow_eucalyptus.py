c8628a239b6c6bf1e6a5ada1bc724f23ca6f6e30
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-07-24 14:46:10 -0500

    i think it works

--- plants/rainbow_eucalyptus.py
+++ plants/rainbow_eucalyptus.py
@@ -4,5 +4,5 @@ from .properties import Requires_Rainfall
 
 class Rainbow_Eucalyptus(Plant):
     def __init__(self):
-        Plant.__init__(self, "Rainbow Eucalyptus Tree", "All", 8, ["Forest"], "Low", "Shade")
+        Plant.__init__(self, "Rainbow Eucalyptus Tree", "All", 8, "Low", "Shade")
         Requires_Rainfall.__init__(self, True)
