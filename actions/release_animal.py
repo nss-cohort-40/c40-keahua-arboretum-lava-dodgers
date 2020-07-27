2f4b23cc787c278fa15429251e6ea9fa4a83fef3
iamkaleb <kalebdmoran@gmail.com>
2020-07-27 11:53:14 -0500

    only available biomes show

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -1,32 +1,151 @@
 import os
 from animals import *
-from places import *
 
 def choose_biome(animal, arboretum):
     os.system('cls' if os.name == 'nt' else 'clear')
     
     biomes = []
 
-    biomes.extend(arboretum.rivers)
-    biomes.extend(arboretum.swamps)
-    biomes.extend(arboretum.coastlines)
-    biomes.extend(arboretum.grasslands)
-    biomes.extend(arboretum.mountains)
-    biomes.extend(arboretum.forests)
+    if animal.species == "Gold Dust Day Gecko":
 
-    for index, biome in enumerate(biomes):
-        print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+        biomes.extend(arboretum.forests)
 
-    print(f"Release {animal.species.lower()} into which biome?")
-    choice = input(">")
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
 
+        print(f"Release {animal.species.lower()} into which biome?")
 
-    try:
-        biomes[int(choice) - 1].add_animal(animal)
-    except AttributeError:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        raise
-        choose_biome(animal, arboretum)
+        choice = input(">")
+
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
+
+    elif animal.species == "River Dolphin":
+        biomes.extend(arboretum.rivers)
+        biomes.extend(arboretum.coastlines)
+
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+
+        print(f"Release {animal.species.lower()} into which biome?")
+
+        choice = input(">")
+
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
+
+    elif animal.species == "Nene Goose":
+        biomes.extend(arboretum.grasslands)
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+        print(f"Release {animal.species.lower()} into which biome?")
+        choice = input(">")
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
+
+    elif animal.species == "Kīkākapu":
+        biomes.extend(arboretum.swamps)
+        biomes.extend(arboretum.rivers)
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+        print(f"Release {animal.species.lower()} into which biome?")
+        choice = input(">")
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
+
+    elif animal.species == "Pueo":
+        biomes.extend(arboretum.grasslands)
+        biomes.extend(arboretum.forests)
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+        print(f"Release {animal.species.lower()} into which biome?")
+        choice = input(">")
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
+
+    elif animal.species == "Ulae":
+        biomes.extend(arboretum.coastlines)
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+        print(f"Release {animal.species.lower()} into which biome?")
+        choice = input(">")
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
+
+    elif animal.species == "Ope'ape'a":
+        biomes.extend(arboretum.forests)
+        biomes.extend(arboretum.mountains)
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+        print(f"Release {animal.species.lower()} into which biome?")
+        choice = input(">")
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
+
+    elif animal.species == "Happy-Face Spider":
+        biomes.extend(arboretum.swamps)
+        for index, biome in enumerate(biomes):
+            print(f'{index + 1}. {biome.name.capitalize()} {str(biome.id)[:8]} {len(biome.animals)} animals')
+        print(f"Release {animal.species.lower()} into which biome?")
+        choice = input(">")
+        try:
+            biomes[int(choice) - 1].add_animal(animal)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Animal successfully released!")
+            input("Press any key to continue >>")
+        except AttributeError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            raise
+            choose_biome(animal, arboretum)
 
 def release_animal(arboretum):
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -48,30 +167,30 @@ def release_animal(arboretum):
         animal = GoldDustDayGecko()
         choose_biome(animal, arboretum)
 
-    if choice == "2":
+    elif choice == "2":
         animal = RiverDolphin()
         choose_biome(animal, arboretum)
 
-    if choice == "3":
+    elif choice == "3":
         animal = NeneGoose()
         choose_biome(animal, arboretum)
 
-    if choice == "4":
+    elif choice == "4":
         animal = Kikakapu()
         choose_biome(animal, arboretum)
 
-    if choice == "5":
+    elif choice == "5":
         animal = Pueo()
         choose_biome(animal, arboretum)
 
-    if choice == "6":
+    elif choice == "6":
         animal = Ulae()
         choose_biome(animal, arboretum)
 
-    if choice == "7":
+    elif choice == "7":
         animal == Opeapea()
         choose_biome(animal, arboretum)
 
-    if choice == "8":
+    elif choice == "8":
         animal = HappyFaceSpider()
         choose_biome(animal, arboretum)
